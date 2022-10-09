4ab3008c38c20a0b2312d4b8b4f1b5a50699bd86
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-10-09 11:01:42 +0800

    Update rbtree.rs

--- src/rbtree.rs
+++ src/rbtree.rs
@@ -31,8 +31,8 @@ pub type NodeCell<K, V> = Option<Box<Node<K, V>>>;
 pub struct Node<K, V> {
     pub key: K,
     pub val: V,
-    pub left: Option<Box<Node<K, V>>>,
-    pub right: Option<Box<Node<K, V>>>,
+    pub left: NodeCell<K, V>>,
+    pub right: NodeCell<K, V>,
     pub color: Color,
 }
 
@@ -214,7 +214,7 @@ fn delete<K: PartialOrd, V>(mut x: NodeCell<K, V>, key: &K) -> NodeCell<K, V> {
 }
 
 pub struct RedBlackBST<K, V> {
-    pub root: Option<Box<Node<K, V>>>,
+    pub root: NodeCell<K, V>,
 }
 
 impl<K: PartialOrd, V> RedBlackBST<K, V> {
